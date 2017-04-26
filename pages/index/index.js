25b02e373d0044fbabc3c1050194e0f36f533a30
LinNanQuan <linnq@zdnst.com>
2017-04-26 17:32:09 +0800

    使用滑动切换效果

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,17 @@ const duration = 2000
 
 Page({
   data: {
-    currentType: "person"
+    tabs: ['人物', '家族', '历史', '城堡'],
+    types: ['person', 'house', 'history', 'castle'],
+    baseUrl: "https://southernbox.github.io/IceAndFireServer/",
+    currentType: "person",
+    stv: {
+      windowWidth: 0,
+      lineWidth: 0,
+      offset: 0,
+      tStart: false
+    },
+    activeTab: 0
   },
   /** 
     * 监听页面加载 
@@ -15,6 +25,16 @@ Page({
     that.setData({
       hideLoading: false
     })
+
+    try {
+      var res = wx.getSystemInfoSync()
+      this.windowWidth = res.windowWidth;
+      this.data.stv.lineWidth = this.windowWidth / this.data.tabs.length;
+      this.data.stv.windowWidth = res.windowWidth;
+      this.setData({ stv: this.data.stv })
+      this.tabsCount = tabs.length;
+    } catch (e) {
+    }
 
     wx.request({
       url: url.getContent,
@@ -69,5 +89,77 @@ Page({
         // complete
       }
     })
+  },
+  handlerStart(e) {
+    let {clientX, clientY} = e.touches[0];
+    this.startX = clientX;
+    this.tapStartX = clientX;
+    this.tapStartY = clientY;
+    this.data.stv.tStart = true;
+    this.tapStartTime = e.timeStamp;
+    this.setData({ stv: this.data.stv })
+  },
+  handlerMove(e) {
+    let {clientX, clientY} = e.touches[0];
+    let {stv} = this.data;
+    let offsetX = this.startX - clientX;
+    this.startX = clientX;
+    stv.offset += offsetX;
+    if (stv.offset <= 0) {
+      stv.offset = 0;
+    } else if (stv.offset >= stv.windowWidth * (this.tabsCount - 1)) {
+      stv.offset = stv.windowWidth * (this.tabsCount - 1);
+    }
+    this.setData({ stv: stv });
+  },
+  handlerCancel(e) {
+
+  },
+  handlerEnd(e) {
+    let {clientX, clientY} = e.changedTouches[0];
+    let endTime = e.timeStamp;
+    let {tabs, stv, activeTab} = this.data;
+    let {offset, windowWidth} = stv;
+    //快速滑动
+    if (endTime - this.tapStartTime <= 300) {
+      //向左
+      if (Math.abs(this.tapStartY - clientY) < 50) {
+        if (this.tapStartX - clientX > 5) {
+          if (activeTab < this.tabsCount - 1) {
+            this.setData({ activeTab: ++activeTab })
+          }
+        } else {
+          if (activeTab > 0) {
+            this.setData({ activeTab: --activeTab })
+          }
+        }
+        stv.offset = stv.windowWidth * activeTab;
+      } else {
+        //快速滑动 但是Y距离大于50 所以用户是左右滚动
+        let page = Math.round(offset / windowWidth);
+        if (activeTab != page) {
+          this.setData({ activeTab: page })
+        }
+        stv.offset = stv.windowWidth * page;
+      }
+    } else {
+      let page = Math.round(offset / windowWidth);
+      if (activeTab != page) {
+        this.setData({ activeTab: page })
+      }
+      stv.offset = stv.windowWidth * page;
+    }
+    stv.tStart = false;
+    this.setData({ stv: this.data.stv })
+  },
+  _updateSelectedPage(page) {
+    let {tabs, stv, activeTab} = this.data;
+    activeTab = page;
+    this.setData({ activeTab: activeTab })
+    stv.offset = stv.windowWidth * activeTab;
+    this.setData({ stv: this.data.stv })
+  },
+  handlerTabTap(e) {
+    this._updateSelectedPage(e.currentTarget.dataset.index);
   }
 })
